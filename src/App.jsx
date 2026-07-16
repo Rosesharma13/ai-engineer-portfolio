9e4d235ca4779f04838470f4b7d5c755d67989cf
Rose Sharma <sharmaarose138@gmail.com>
2026-07-16 12:00:15 +0530

    Add Experience component to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import CursorTrail from './components/CursorTrail';
 import Hero from './components/Hero';
+import Experience from './components/Experience';
 import Projects from './components/Projects';
 import Certifications from './components/Certifications';
 import ChatBot from './components/ChatBot';
@@ -10,6 +11,7 @@ export default function App() {
     <div className="relative min-h-screen selection:bg-blue-500/30 selection:text-blue-200">
       <CursorTrail />
       <Hero />
+      <Experience />
       <Projects />
       <Certifications />
       <ChatBot />
